ca87354d0dca0d0bf831c72cf1592b58ea633bd0
KATT <alexander@n1s.se>
2023-06-02 01:23:28 +0200

    rm som barrel files

---
+++ README.md
@@ -0,0 +1,1 @@
+Streamed hydration with `"use client"` components using `@tanstack/react-query`.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
+"use client";
 import { Suspense } from "react";
-import { MyComponent } from "./MyComponent";
+import { useQuery } from "@tanstack/react-query";
+
+function useWaitQuery(props: { wait: number }) {
+  const query = useQuery({
+    queryKey: ["wait", props.wait],
+    queryFn: async () => {
+      const url = `http://localhost:3000/api/wait?wait=${props.wait}`;
+      console.log("fetching", url);
+      const res: string = await (
+        await fetch(url, {
+          cache: "no-store",
+        })
+      ).json();
+      return res;
+    },
+    suspense: true,
+  });
+
+  return [query.data as string, query] as const;
+}
+
+export function MyComponent(props: { wait: number }) {
+  const [data] = useWaitQuery(props);
+
+  return <div>result: {data}</div>;
+}
 
 export default function MyPage() {
   return (
